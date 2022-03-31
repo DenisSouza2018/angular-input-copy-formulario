2746e6d979182118fbbe7ff363e2e38bb758c53a
DenisSouza2018 <denis.souza.rosa@gmail.com>
2022-03-31 10:53:22 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-input-example-wjcswy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-input-example-wjcswy)
---
+++ src/app/app.component.ts
@@ -0,0 +1,85 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  ngOnInit() {
+    console.log(true);
+  }
+
+  obj1 = [
+    { value: '', edit: false },
+    { value: '', edit: false },
+    { value: '', edit: false },
+    { value: '', edit: false },
+    { value: '', edit: false },
+    { value: '', edit: false },
+  ];
+
+  obj2 = [
+    { value: '', edit: false },
+    { value: '', edit: false },
+    { value: '', edit: false },
+    { value: '', edit: false },
+    { value: '', edit: false },
+    { value: '', edit: false },
+  ];
+
+  obj3 = [
+    { value: '', edit: false },
+    { value: '', edit: false },
+    { value: '', edit: false },
+    { value: '', edit: false },
+    { value: '', edit: false },
+    { value: '', edit: false },
+  ];
+
+  name1 = 'Angular ';
+  name2;
+
+  changeEdit(type, index) {
+    switch (type) {
+      case 1:
+        this.obj1[index].edit = true;
+        break;
+
+      case 2:
+        this.obj2[index].edit = true;
+        break;
+    }
+  }
+
+  copy(type) {
+    switch (type) {
+      case 1:
+        this.resetEdit(this.obj2);
+        this.obj1.forEach((e, index) => {
+          if (e.edit) {
+            this.obj2[index].edit = e.edit;
+            this.obj2[index].value = e.value;
+          }
+        });
+        break;
+
+      case 2:
+        this.resetEdit(this.obj3);
+        this.resetEdit(this.obj1);
+        this.obj2.forEach((e, index) => {
+          if (e.edit) {
+            this.obj3[index].edit = e.edit;
+            this.obj3[index].value = e.value;
+          }
+        });
+        break;
+    }
+  }
+
+  resetEdit(obj) {
+    return obj.forEach((e) => {
+      e.edit = false;
+    });
+  }
+}
